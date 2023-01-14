ac66e22e4b1077092bfa73ed755a1087bd0f6374
YuriSS <yuri.santos@db.tec.br>
2023-01-14 10:26:25 -0300

    feat: removed .js files

--- src/infrastructure/webserver/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("@infrastructure/webserver/express");
-const port = Number(process.env.PORT) || 3000;
-express_1.app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
